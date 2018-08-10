01e539ef8ba9d0c842d44ef3df496895de184482
Livia Vasconcelos <ltenoriovasconcelos@gmail.com>
2018-08-10 17:04:38 -0300

    master Add initial structure for project
    -
    
    Signed-off-by: Livia Vasconcelos <ltenoriovasconcelos@gmail.com>

---
+++ IMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Stant 02 on 10/08/18.
+//  Copyright © 2018 Liu Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
