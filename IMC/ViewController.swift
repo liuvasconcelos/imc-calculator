293fc347baa189367e0a08d421184846267e87a1
Livia Vasconcelos <ltenoriovasconcelos@gmail.com>
2018-08-20 15:06:17 -0300

    master Add aditional logis for calculate icm and show on screen
    -
    
    Signed-off-by: Livia Vasconcelos <ltenoriovasconcelos@gmail.com>

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weight: UITextField!
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var resultView: UIView!
+    
+    var imc: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 
-
+    @IBAction func calculeButton(_ sender: Any) {
+        if let weightValue = Double(weight.text!), let heightValue = Double(height.text!) {
+            let height = heightValue/100
+            imc = weightValue / (height * height)
+            showResults()
+        }
+    }
+    
+    private func showResults() {
+        var resultToShow: String = ""
+        var image: String = ""
+        
+        switch imc {
+            case 0..<16:
+                resultToShow = "Magreza"
+                image = "abaixo"
+            case 16..<18.5:
+                resultToShow = "Abaixo do peso"
+                image = "abaixo"
+            case 18.5..<25:
+                resultToShow = "Peso Ideal"
+                image = "ideal"
+            case 25..<30:
+                resultToShow = "Sobrepeso"
+                image = "sobre"
+            default:
+                resultToShow = "Obesidade"
+                image = "obesidade"
+        }
+        
+        result.text = "\(Int(imc))%: \(resultToShow)"
+        resultImage.image = UIImage(named: image)
+        resultView.isHidden = false
+        view.endEditing(true)
+    }
 }
 
